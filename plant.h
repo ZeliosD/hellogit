fe6370c15aad71e1e4c52aecf2396c29f30daa73
ZeliosD <D-HPSN@hotmail.com>
2018-01-31 11:33:15 -0400

    Added header file

---
+++ plant.h
@@ -0,0 +1,30 @@
+#ifndef PLANT_H
+#define STUDENT_H
+class plant() {
+	private:
+		float height;
+		std::string species;
+	public:
+		plant(float h, std::string s) {
+			height = h;
+			species = s;
+		}
+
+		void setSpecies(std::string newSpeciesName) {
+			species = newSpeciesname;
+			return;
+		}
+
+		std::string getSpecies() {
+			return species;
+		}
+
+		void setHeight(float newHeight) {
+			height = newHeight;
+			return;
+		}
+
+		float getHeight() {
+			return height;
+		}
+};
